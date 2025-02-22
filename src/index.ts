1c06af1f369e580204c37aacdd0754d42346a8cf
Simon-Fontaine <74004726+Simon-Fontaine@users.noreply.github.com>
2025-02-22 13:41:15 +0100

    feat: added package index

---
+++ src/index.ts
@@ -0,0 +1,71 @@
+import type { PrismaClient } from "@prisma/client";
+import { Ratelimit } from "@upstash/ratelimit";
+import { buildConfig } from "./config";
+import {
+  type AegisAuthConfig,
+  type AegisContext,
+  type Endpoints,
+  defaultEndpoints,
+} from "./types";
+import { processRequest } from "./utils";
+
+export class AegisAuth {
+  private config: AegisAuthConfig;
+  private prisma: PrismaClient;
+  private endpoints: Endpoints = {};
+
+  private async createContext(headers: Headers): Promise<AegisContext> {
+    const response = await processRequest(
+      this.prisma,
+      this.config,
+      this.endpoints,
+      headers,
+    );
+
+    if (!response.success) {
+      throw new Error(`Failed to create context: ${response.error.message}`);
+    }
+
+    return response.data;
+  }
+
+  private initializeEndpoints() {
+    const { rateLimit } = this.config;
+    if (!rateLimit.enabled || !rateLimit.redis) return;
+
+    try {
+      for (const endpoint of defaultEndpoints) {
+        const endpointConfig = rateLimit.endpoints[endpoint];
+        if (!endpointConfig || !endpointConfig.enabled) continue;
+        this.endpoints[endpoint] = new Ratelimit({
+          redis: rateLimit.redis,
+          limiter: Ratelimit.slidingWindow(
+            endpointConfig.maxRequests,
+            `${endpointConfig.windowSeconds} s`,
+          ),
+          prefix: `${rateLimit.prefix}:${endpoint}`,
+        });
+      }
+    } catch (error) {
+      this.config.logger?.error("Failed to initialize endpoints.", {
+        error: error instanceof Error ? error.message : String(error),
+      });
+    }
+  }
+
+  constructor(prisma: PrismaClient, userConfig?: Partial<AegisAuthConfig>) {
+    this.prisma = prisma;
+
+    const response = buildConfig(userConfig ?? {});
+    if (!response.success) {
+      throw new Error(`Failed to create config: ${response.error.message}`);
+    }
+
+    this.config = response.data;
+    this.initializeEndpoints();
+  }
+
+  getConfig(): AegisAuthConfig {
+    return this.config;
+  }
+}
